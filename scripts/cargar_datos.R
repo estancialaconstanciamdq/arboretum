68c053657918ede80315bfd82261eb28580204b2
Juan Zurita <jzurita@ed.ac.uk>
2026-06-19 15:50:53 +0100

    initial commit

---
+++ scripts/cargar_datos.R
@@ -0,0 +1,31 @@
+library(readxl)
+library(dplyr)
+
+archivo <- "data/Arboretum_Master.xlsx"
+
+arboles <- read_excel(
+  archivo,
+  sheet = "Árboles"
+)
+
+aves <- read_excel(
+  archivo,
+  sheet = "Aves"
+)
+
+puntos <- read_excel(
+  archivo,
+  sheet = "Puntos de interés"
+)
+
+# eliminar puntos sin coordenadas
+
+puntos <- puntos %>%
+  filter(
+    latitud != "...",
+    longitud != "..."
+  ) %>%
+  mutate(
+    latitud = as.numeric(latitud),
+    longitud = as.numeric(longitud)
+  )
